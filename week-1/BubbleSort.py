c62bf2b5f5e3c4ed4c793965a849ba6e9a346571
MastewalB <se.mastewal.belete@gmail.com>
2021-12-10 08:07:05 +0300

    Bubble Sort

---
+++ week-1/BubbleSort.py
@@ -0,0 +1,18 @@
+def count_swaps(a):
+    count = 0
+
+    for i in range(len(a)):
+        for j in range(len(a) - 1):
+            if a[j] > a[j+1]:
+                a[j], a[j+1] = a[j+1], a[j]
+                count += 1
+
+    print("Array is sorted in ", count, " swaps.")
+    print("First Element ", a[0])
+    print("Last Element, ", a[len(a) - 1])
+
+
+a = [6, 4, 1]
+a = [1, 2, 3]
+a = [1]
+count_swaps(a)
