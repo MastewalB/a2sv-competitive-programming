9da97174fe8572d62d6bd63d9b659099a6407a1c
Mastewal B <se.mastewal.belete@gmail.com>
2021-12-10 18:44:16 +0300

    selection modified

--- week-1/SelectionSort.py
+++ week-1/SelectionSort.py
@@ -1,17 +1,13 @@
 class Solution:
-    def select(self, arr, i):
-        minimum = i
-        for j in range(i + 1, len(arr)):
-            if arr[j] < arr[minimum]:
-                minimum = j
-        print(minimum)
-        return minimum
 
     def selectionSort(self, arr, n):
-        for i in range(n - 1):
-            minimum = self.select(arr, i + 1)
-            if arr[minimum] < arr[i]:
-                arr[i], arr[minimum] = arr[minimum], arr[i]
+        for i in range(n):
+            minimum = i
+            for j in range(i + 1, len(arr)):
+                if arr[j] < arr[minimum]:
+                    minimum = j
+
+            arr[i], arr[minimum] = arr[minimum], arr[i]
             print(arr)
         return arr
 
