4b9dd3c6484a76ad0d785a2997ba38465818e96c
MastewalB <se.mastewal.belete@gmail.com>
2021-12-12 02:50:32 +0300

    Insertion Sort modified

--- week-1/InsertionSort.py
+++ week-1/InsertionSort.py
@@ -4,29 +4,42 @@
 def print_arr(arr):
     for i in range(len(arr)):
         print(arr[i], end=" ")
+    print()
 
 
 def insertion_sort(n, arr):
-    if len(arr) == 1:
+
+    for j in range(1, len(arr)):
+        print("Currenty Checking - ", arr[j], " - - - ", end=" ")
+
+        i = j - 1
+        if arr[j] >= arr[i]:
+            print_arr(arr)
+            continue
+        else:
+
+            # Go back until we find smaller number or to the beginning
+            while i >= 0 and arr[j] < arr[i]:
+                i -= 1
+
+            # put the number to temporary variable
+            temp = arr[j]
+            # shift every element to the right of the array to make room for the number
+            for k in range(j, i + 1, -1):
+                arr[k] = arr[k - 1]
+            arr[i + 1] = temp
         print_arr(arr)
-        print()
-        return
-    current_element = arr[n - 1]
-    i = n - 2
-    while arr[i] > current_element and i >= 0:
 
-        arr[i + 1] = arr[i]
-        print_arr(arr)
-        print()
-
-        i -= 1
-
-    arr[i + 1] = current_element
+    print("Final - ", end=" ")
     print_arr(arr)
     print()
 
 
-arr = [1, 2, 4, 5, 3]
-arr = [2, 4, 6, 8, 3]
-arr = [2, 3, 4, 5, 6, 7, 8, 9, 10, 1]
-insertion_sort(10, arr)
+arrays = [
+    [1, 2, 4, 5, 3],
+    [2, 3, 4, 5, 6, 7, 8, 9, 10, 1],
+    [1, 4, 3, 5, 6, 2],
+    [0, 10, -1]
+]
+for array in arrays:
+    insertion_sort(len(array), array)
