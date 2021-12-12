ae4deae680726cd41125297da704bf728442a9ff
MastewalB <se.mastewal.belete@gmail.com>
2021-12-12 10:02:31 +0300

    K closest

---
+++ week-1/973-KClosestPointsToOrigin.py
@@ -0,0 +1,35 @@
+import math
+
+
+def calculate_distance(point_1, point_2):
+    d = math.sqrt(((point_1[0] - point_2[0]) ** 2) +
+                  ((point_1[1] - point_2[1]) ** 2)
+                  )
+    return d
+
+
+# print(calculate_distance([-2, 4], [0, 0]))
+# print(calculate_distance([5, -1], [0, 0]))
+
+
+def k_closest(points, k):
+    origin = [0, 0]
+    for i in range(k):
+
+        min_distance = calculate_distance(points[i], origin)
+        min_distance_index = i
+
+        for j in range(i + 1, len(points)):
+            distance = calculate_distance(points[j], origin)
+
+            if distance < min_distance:
+
+                min_distance = distance
+                min_distance_index = j
+            points[i], points[min_distance_index] = points[min_distance_index], points[i]
+    return points[:k]
+
+
+points = [[3, 3], [5, -1], [-2, 4]]
+k = 2
+print(k_closest(points, k))
