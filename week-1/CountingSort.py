ce6d450b9691753c354a63195fd6d5b258fac810
MastewalB <se.mastewal.belete@gmail.com>
2021-12-12 03:27:09 +0300

    Counting sort 2

--- week-1/CountingSort.py
+++ week-1/CountingSort.py
@@ -1,9 +1,28 @@
+import random
+
+
 def countingSort(arr):
-    lst = [0] * 100
+    count_arr = [0] * 100
     for i in range(len(arr)):
-        lst[arr[i]] += 1
-    return lst
+        count_arr[arr[i]] += 1
+
+    # Place a cursor on the beginning of array
+    cursor = 0
+    for k in range(len(count_arr)):
+        for i in range(count_arr[k]):
+
+            arr[i + cursor] = k
+        cursor += count_arr[k]
+
+    for i in range(len(arr)):
+        print(arr[i], end=" ")
+    print()
+    return arr
 
 
-arr = [0, 1, 0, 0]
-print(countingSort(arr))
+arr = [0, 1, 0, 1, 0]
+for i in range(100):
+    arr.append(random.randint(0, 99))
+
+
+countingSort(arr)
