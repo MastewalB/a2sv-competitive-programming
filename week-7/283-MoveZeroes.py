606461f8251dc46b0762103bc2b26d32091a3edc
MastewalB <se.mastewal.belete@gmail.com>
2022-01-28 10:17:24 +0300

    Move Zeroes Modified

--- week-7/283-MoveZeroes.py
+++ week-7/283-MoveZeroes.py
@@ -13,3 +13,18 @@ class Solution:
                 last_non_zero += 1
         for j in range(last_non_zero, len(nums)):
             nums[j] = 0
+
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        if len(nums) <= 1:
+            return
+        last_non_zero = 0
+
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[last_non_zero], nums[i] = nums[i], nums[last_non_zero]
+                last_non_zero += 1
