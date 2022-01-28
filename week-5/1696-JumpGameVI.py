8772ca865cac4d4607ac0b857d98c75339f303c2
MastewalB <se.mastewal.belete@gmail.com>
2022-01-28 11:00:36 +0300

    Jump Game Optimized

--- week-5/1696-JumpGameVI.py
+++ week-5/1696-JumpGameVI.py
@@ -11,3 +11,18 @@ class Solution:
                     Max = ret_val
         result += Max
         return result
+
+
+class Solution:
+    def maxResult(self, nums: List[int], k: int) -> int:
+        queue = deque()
+        queue.append(nums[0])
+
+        for i in range(1, len(nums)):
+            while queue and queue[0] < (i - k):
+                queue.popleft()
+            nums[i] += nums[queue[0]]
+            while queue and nums[i] >= nums[queue[-1]]:
+                queue.pop()
+            queue.append(i)
+        return nums[-1]
