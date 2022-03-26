d31a84f5e0f8d10a4b3cff1ef80b32731c907fff
MastewalB <se.mastewal.belete@gmail.com>
2022-03-26 09:28:45 +0300

    Sum Of Subarray Ranges

---
+++ DailyQuestions/2104-SumOfSubarrayRanges.py
@@ -0,0 +1,14 @@
+class Solution:
+    def subArrayRanges(self, nums: List[int]) -> int:
+        total = 0
+
+        for i in range(len(nums) - 1):
+            min_A = max_A = nums[i]
+
+            for j in range(i + 1, len(nums)):
+                if nums[j] < min_A:
+                    min_A = nums[j]
+                if nums[j] > max_A:
+                    max_A = nums[j]
+                total += max_A - min_A
+        return total
