54871d8009f883b3a68d1f2f1783d8d82ec65d9a
Mastewal Belete <52622856+MastewalB@users.noreply.github.com>
2022-08-12 10:42:37 +0300

    Time: 2342 ms (96.94%), Space: 31.5 MB (62.76%) - LeetHub

--- 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.py
+++ 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.py
@@ -5,47 +5,24 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def findRoot(self, descriptions):
-        nodes = set()
-        for parent, child, side in descriptions:
-            nodes.add(parent)
-        for parent, child, side in descriptions:
-            if child in nodes:
-                nodes.remove(child)
-        return list(nodes)[0]
-        
-    def buildGraph(self, descriptions):
-        nodes = defaultdict(list)
-        
-        for parent, child, side in descriptions:
-            if not nodes[parent]:
-                nodes[parent] = [None, None]
-            nodes[parent][side] = child 
-        
-        return nodes
-    
-        
     def createBinaryTree(self, descriptions: List[List[int]]) -> Optional[TreeNode]:
-        graph = self.buildGraph(descriptions)
-        nodeGraph = defaultdict(TreeNode)
-        root = self.findRoot(descriptions)
-                
-        def dfs(node):
-            if not node:
-                return 
-            if node not in nodeGraph:
-                            
-                root = TreeNode(node)
-                if graph[node]:
-                    root.left = dfs(graph[node][1])
-                    root.right = dfs(graph[node][0])
-                nodeGraph[node] = root
-            
-            return nodeGraph[node]
+        treeDict = defaultdict(TreeNode)
+        children = set()
+        
         
         for parent, child, side in descriptions:
-            if parent not in nodeGraph:
-                dfs(parent)
             
+            treeDict[parent].val = parent
+            treeDict[child].val = child
+            
+            if side == 1:
+                treeDict[parent].left = treeDict[child]
+            else:
+                treeDict[parent].right = treeDict[child]
+            
+            children.add(child)
         
-        return nodeGraph[root]
+        
+        for parent, child, side in descriptions:
+            if parent not in children:
+                return treeDict[parent]
