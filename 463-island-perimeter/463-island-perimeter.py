0a7fbcac91c1862d17d5fd72b4f8c8a8afbfc400
Mastewal Belete <52622856+MastewalB@users.noreply.github.com>
2022-08-12 22:46:38 +0300

    Time: 869 ms (48.44%), Space: 14.3 MB (48.17%) - LeetHub

--- 463-island-perimeter/463-island-perimeter.py
+++ 463-island-perimeter/463-island-perimeter.py
@@ -1,27 +1,20 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
-        visited = set()
-        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-        inBound = lambda row, col: 0 <= row < len(grid) and 0 <= col < len(grid[0])
+        perimeter = 0
         
-        def dfs(row, col):
-            if not inBound(row, col) or grid[row][col] == 0:
-                return 1
-            
-            visited.add((row, col))
-            perimeter = 0
-            for x, y in directions:
-                n_r, n_c = row + x, col + y
-                
-                if (n_r, n_c) not in visited:
-                    perimeter += dfs(n_r, n_c)
-            
-            return perimeter
         
-        start = None
         for i in range(len(grid)):
-            for j in range(len(grid[0])):
+            for j in range(len(grid[i])):
                 if grid[i][j]:
-                    start = (i, j)
+                    perimeter += 4
+                
+                    if i > 0:
+                        perimeter -= grid[i - 1][j]
+                    if i < len(grid) - 1:
+                        perimeter -= grid[i + 1][j]
+                    if j > 0:
+                        perimeter -= grid[i][j - 1]
+                    if j < len(grid[i]) - 1:
+                        perimeter -= grid[i][j + 1]
         
-        return dfs(start[0], start[1])
+        return perimeter
