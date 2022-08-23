a6e1e6f1de25196b5f42db1b7fa4a74ce729190c
Mastewal Belete <52622856+MastewalB@users.noreply.github.com>
2022-08-23 09:21:39 +0300

    Time: 5379 ms (5.02%), Space: 53.7 MB (35.82%) - LeetHub

--- 2055-plates-between-candles/2055-plates-between-candles.py
+++ 2055-plates-between-candles/2055-plates-between-candles.py
@@ -1,38 +1,33 @@
 class Solution:
     def platesBetweenCandles(self, s: str, queries: List[List[int]]) -> List[int]:
         candles = []
-        plateSum = [0] * len(s) 
         output = []
         
         def findNearest(index, direction):
-            best = None
+            best, pos = None, None
             left, right = 0, len(candles) - 1
             
             while left <= right:
                 mid = left + right >> 1  
                 if candles[mid] == index:
-                    return candles[mid]
+                    return candles[mid], mid
                 elif candles[mid] < index:
                     if direction == -1:
-                        best = candles[mid]
+                        best, pos = candles[mid], mid
                     left = mid + 1
                 else:
                     if direction == 1:
-                        best = candles[mid]
+                        best, pos = candles[mid], mid
                     right = mid - 1
-            return best
+            return best, pos
         
-        last = None
         for i in range(len(s)):
             if s[i] == "|":
                 candles.append(i)
-                if last != None:
-                    plateSum[i] += i - last - 1
-                    plateSum[i] += plateSum[last]
-                last = i 
         
         for l, r in queries:
-            l, r = findNearest(l, 1), findNearest(r, -1)
-            output.append(plateSum[r] - plateSum[l]) if (l != None and r != None and l < r) else output.append(0)
+            l, lIndex = findNearest(l, 1)
+            r, rIndex = findNearest(r, -1)
+            output.append((r - l) - (rIndex - lIndex)) if (l != None and r != None and l < r) else output.append(0)
         
         return output
