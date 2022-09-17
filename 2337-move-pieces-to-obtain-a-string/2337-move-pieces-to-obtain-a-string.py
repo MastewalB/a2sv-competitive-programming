8599f3e265b40d6ad232b1f45b8dbd316061983e
Mastewal Belete <52622856+MastewalB@users.noreply.github.com>
2022-09-17 17:21:45 +0300

    Time: 417 ms (65.21%), Space: 15.3 MB (47.67%) - LeetHub

--- 2337-move-pieces-to-obtain-a-string/2337-move-pieces-to-obtain-a-string.py
+++ 2337-move-pieces-to-obtain-a-string/2337-move-pieces-to-obtain-a-string.py
@@ -1,29 +1,21 @@
 class Solution:
     def canChange(self, start: str, target: str) -> bool:
         N = len(start)
-        start_left = []
-        start_right = []
-        target_left = []
-        target_right = []
-        
-        
         if start.replace("_", "") != target.replace("_", ""):
             return False
         
-        for i in range(N):
-            if target[i] == 'R':
-                target_right.append(i)
-            elif target[i] == 'L':
-                target_left.append(i)
-            if start[i] == 'L':
-                start_left.append(i)
-            elif start[i] == 'R':
-                start_right.append(i)
+        i = j = 0
         
-        for i in range(max(len(start_right), len(start_left))):
-            if i < len(start_right) and start_right[i] > target_right[i]:
-                return False
-            if i < len(start_left) and start_left[i] < target_left[i]:
-                return False
+        while i < N and j < N:
+            while i < N and start[i] == '_':
+                i += 1
+            while j < N and target[j] == '_':
+                j += 1
+            
+            if i < N and ((start[i] == 'L' and i < j) or (start[i] == 'R' and i > j)):
+                return False 
+            
+            i += 1
+            j += 1
         
         return True
