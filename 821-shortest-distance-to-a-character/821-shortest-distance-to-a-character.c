d958ac78109d003e12ea0950574a3470d002f897
Mastewal Belete <52622856+MastewalB@users.noreply.github.com>
2022-09-26 01:42:30 +0300

    Time: 11 ms (59.62%), Space: 6.2 MB (94.23%) - LeetHub

--- 821-shortest-distance-to-a-character/821-shortest-distance-to-a-character.c
+++ 821-shortest-distance-to-a-character/821-shortest-distance-to-a-character.c
@@ -6,25 +6,19 @@
 int* shortestToChar(char * s, char c, int* returnSize){
     int N = strlen(s);
     int * res = (int *)malloc(sizeof(int) * N);
-    
-    int prev = N + 1;
+    int prev = INT_MAX;
     
     for(int i = 0; i < N; i++){
-        if(s[i] == c){
+        if(s[i] == c)
             prev = i;
-            res[i] = 0;
-        }
-        else
-            res[i] = (prev < N) ? i - prev : INT_MAX;
+        res[i] = abs(prev - i);
     }
     
-    prev = N + 1;
+    prev = INT_MAX;
     for(int i = N - 1;  i >= 0; i--){
         if(s[i] == c)
             prev = i;
-        else
-            res[i] = (prev < N) ? MIN(res[i], prev - i) : res[i];
-            
+        res[i] = MIN(res[i], abs(prev - i));            
     }
     
     *returnSize = N;
