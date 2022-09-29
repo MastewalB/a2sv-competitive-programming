7cdc2013a572a94aa324af72b802eb6c46f234c5
Mastewal Belete <52622856+MastewalB@users.noreply.github.com>
2022-09-29 17:33:00 +0300

    Time: 649 ms (44.71%), Space: 20.8 MB (91.76%) - LeetHub

---
+++ 962-maximum-width-ramp/962-maximum-width-ramp.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxWidthRamp(self, nums: List[int]) -> int:
+        stack = [0]
+        for i in range(1, len(nums)):
+            if nums[stack[-1]] >= nums[i]:
+                stack.append(i)
+        
+        ans = 0
+        for i in range(len(nums) - 1, -1, -1):
+            while stack and nums[stack[-1]] <= nums[i]:
+                ans = max(ans, i - stack.pop())
+        return ans
