80cb27d4d62a9c0dde48f66a79c552237e03aba7
MastewalB <se.mastewal.belete@gmail.com>
2022-10-01 02:01:05 +0300

    Refactoring

--- contest/Contest 16/A.py
+++
@@ -1,9 +0,0 @@
-n, h = list(map(int, input().split()))
-minWidth = 0
-heights = list(map(int, input().split()))
-for i in range(n):
-    if heights[i] > h:
-        minWidth += 2
-    else:
-        minWidth += 1
-print(minWidth)
--- contest/Next-Phase/Contest-1/A.py
+++
@@ -1,6 +0,0 @@
-
-lineland = list(map(int, input().split()))
-lineland.sort()
-
-minDist = lineland[1] - lineland[0] + lineland[2] - lineland[1]
-print(minDist)
