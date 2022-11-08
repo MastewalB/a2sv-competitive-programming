f66828f2c45cade3281e5123cf8af03efcaf92b6
Mastewal Belete <52622856+MastewalB@users.noreply.github.com>
2022-11-08 19:25:19 +0300

    Time: 79 ms (10.51%), Space: 14 MB (82.41%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,15 +1,8 @@
 class Solution:
-    def __init__(self):
-        self.subset = []
-    def generate(self, nums, index, S):
-        if index == len(nums):
-            self.subset.append(S[::])
-        else:
-            self.generate(nums, index + 1, S)
-            S.append(nums[index])
-            self.generate(nums, index + 1, S)
-            S.pop()
-        
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        self.generate(nums, 0, [])
-        return self.subset
+        S = [[]]
+        for num in nums:
+            size = len(S)
+            for i in range(size):
+                S.append(S[i] + [num])
+        return S
