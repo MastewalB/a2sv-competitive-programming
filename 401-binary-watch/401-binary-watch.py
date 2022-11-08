513437652fb4599ad6100c799b6ec2a5822826ce
Mastewal Belete <52622856+MastewalB@users.noreply.github.com>
2022-11-08 13:08:49 +0300

    Time: 137 ms (5.04%), Space: 13.9 MB (72.02%) - LeetHub

--- 401-binary-watch/401-binary-watch.py
+++ 401-binary-watch/401-binary-watch.py
@@ -1,17 +1,16 @@
 class Solution:
-    def backtrack(self, turnedOn, hour, minute, watch, startIndex):
-        if turnedOn == 0:
-            return ["%d:%02d" % (hour, minute)]
-
-        answer = []
-        for i in range(startIndex, len(watch)):
-            if i < 4 and hour + watch[i] < 12:
-                answer.extend(self.backtrack(turnedOn - 1, hour + watch[i], minute, watch, i + 1))
-            if i >= 4 and minute + watch[i] < 60:
-                answer.extend(self.backtrack(turnedOn - 1, hour, minute + watch[i], watch, i + 1))
-        return answer
-        
-        
+    def countBit(self, N):
+        count = 0
+        for i in range(32):
+            count += ((1 << i) & N) != 0
+        return count
+    
     def readBinaryWatch(self, turnedOn: int) -> List[str]:
-        watch = [1, 2, 4, 8, 1, 2, 4, 8, 16, 32]
-        return self.backtrack(turnedOn, 0, 0, watch, 0)
+        answer = []
+        
+        for h in range(12):
+            for m in range(60):
+                if self.countBit(h) + self.countBit(m) == turnedOn:
+                    answer.append("%d:%02d" % (h, m))
+        
+        return answer
