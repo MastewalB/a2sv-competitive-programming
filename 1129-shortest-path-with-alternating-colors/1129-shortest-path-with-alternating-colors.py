2396897c572341124df2d347d39e7f8c16aba3f1
Mastewal Belete <52622856+MastewalB@users.noreply.github.com>
2023-02-11 12:54:46 +0300

    Time: 105 ms (31.52%), Space: 14.2 MB (31.23%) - LeetHub

--- 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.py
+++ 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.py
@@ -1,44 +1,29 @@
 class Solution:
     def shortestAlternatingPaths(self, n: int, redEdges: List[List[int]], blueEdges: List[List[int]]) -> List[int]:
-        def buildGraph(edges):
-            graph = collections.defaultdict(set)
-            for x, y in edges:
-                graph[x].add(y)
+        def buildGraph():
+            graph = [[[], []] for _ in range(n)]
+            for s, d in redEdges:
+                graph[s][0].append(d)
+            for s, d in blueEdges:
+                graph[s][1].append(d)
             
-            return graph
+            return graph 
         
-        redGraph = buildGraph(redEdges)
-        blueGraph = buildGraph(blueEdges)
-        path = [float("inf") for _ in range(n)]
-        
-        def bfs(initial):
-            queue = collections.deque([0])
-            visited = set()
-            level = 0
-            
-            while queue:
-                size = len(queue)
-                for _ in range(size):
-                    node = queue.popleft()
-                    path[node] = min(path[node], level)
-                    if initial:
-                        for neigh in redGraph[node]:
-                            if (node, neigh, initial) not in visited:
-                                visited.add((node, neigh, initial))
-                                queue.append(neigh)
-                    else:
-                        for neigh in blueGraph[node]:
-                            if (node, neigh, initial) not in visited:
-                                visited.add((node, neigh, initial))
-                                queue.append(neigh)
-                
-                level += 1
-                initial = 1 - initial
-        
-        bfs(0)
-        bfs(1)
-        
+        graph = buildGraph()
+        path = [[float("inf"), float("inf")] for _ in range(n)]
+        path[0] = [0, 0]
+
+        def dfs(u, color, distance):
+            for v in graph[u][color]:
+                if distance + 1 < path[v][color]:
+                    path[v][color] = distance + 1
+                    dfs(v, 1 - color, path[v][color])
+
+        dfs(0, 0, 0)
+        dfs(0, 1, 0)
+
         for i in range(n):
+            path[i] = min(path[i])
             if path[i] == float("inf"):
                 path[i] = -1
         return path
