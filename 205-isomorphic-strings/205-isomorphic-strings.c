7113ca1ffcdd7107ad33d880ea963c3a09bbdfb9
Mastewal Belete <52622856+MastewalB@users.noreply.github.com>
2023-02-22 10:47:49 +0300

    Time: 7 ms (32.60%), Space: 6 MB (39.04%) - LeetHub

---
+++ 205-isomorphic-strings/205-isomorphic-strings.c
@@ -0,0 +1,25 @@
+bool isIsomorphic(char * s, char * t){
+    int charSet[129];
+    int mapSet[129];
+    for(int i = 0; i < 129; i++){
+        charSet[i] = -1;
+        mapSet[i] = -1;
+    }
+    
+    while(*s){
+        if(charSet[*s] >= 0){
+            if(charSet[*s] != *t){
+                return false;
+            }
+        }else if(mapSet[*t] >= 0){
+            return false;
+        }
+        else{
+            charSet[*s] = *t;
+            mapSet[*t] = *s;
+        }
+        s++;
+        t++;
+    }
+    return true;
+}
