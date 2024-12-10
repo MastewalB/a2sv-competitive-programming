0c5fb0af7f8ff14e523861550b99d8a2f0dec520
Mastewal Belete <52622856+MastewalB@users.noreply.github.com>
2024-12-10 15:28:54 +0300

    3427-SPECIAL-ARRAY-II Stats: Time: 1 ms (77.78%), Space: 22.3 MB (17.86%) - LeetHub

--- 3427-special-array-ii/3427-special-array-ii.go
+++ 3427-special-array-ii/3427-special-array-ii.go
@@ -1,22 +1,30 @@
 func isArraySpecial(nums []int, queries [][]int) []bool {
-    n, q := len(nums), len(queries)
-    prefixSum := make([]int, n)
-    res := slices.Repeat([]bool{true}, q)
+    deviant, n, q := []int{}, len(nums), len(queries)
+    res := make([]bool, q)
 
-    prefixSum[0] = 0
-    for i := 1; i < n; i++ {
-        if nums[i] % 2 == nums[i - 1] % 2 {
-            prefixSum[i] = prefixSum[i - 1] + 1
-        } else {
-            prefixSum[i] = prefixSum[i - 1]
+    for i := 0; i < n - 1; i++ {
+        if nums[i] % 2 == nums[i + 1] % 2 {
+            deviant = append(deviant, i)
         }
     }
 
-    for i := 0; i < q; i++ {
-        l, r := queries[i][1], queries[i][0]
-        if prefixSum[l] - prefixSum[r] > 0 {
-            res[i] = false
+    for k, v := range queries {
+        low, high, flag := v[0], v[1], true
+        l, r := 0, len(deviant) - 1
+        for l <= r {
+            mid := (l + r) / 2
+            if deviant[mid] < low {
+                l = mid + 1
+            } else {
+                if deviant[mid] >= high {
+                    r = mid - 1
+                } else {
+                    flag = false
+                    break
+                }
+            }
         }
+        res[k] = flag
     }
     return res
 }
