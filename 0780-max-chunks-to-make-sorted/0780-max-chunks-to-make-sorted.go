f4e15726150b51551ef312efd1bf095b72172920
Mastewal Belete <52622856+MastewalB@users.noreply.github.com>
2024-12-19 15:09:09 +0300

    0780-MAX-CHUNKS-TO-MAKE-SORTED Stats: Time: 0 ms (100%), Space: 3.9 MB (5.26%) - LeetHub

--- 0780-max-chunks-to-make-sorted/0780-max-chunks-to-make-sorted.go
+++ 0780-max-chunks-to-make-sorted/0780-max-chunks-to-make-sorted.go
@@ -1,17 +1,9 @@
 func maxChunksToSorted(arr []int) int {
-    chunk, arrCopy := 0, make([]int, 0)
+    chunk, _max := 0, arr[0]
 
     for k, v := range arr {
-        arrCopy = append(arrCopy, v)
-        if isValid(arrCopy, k) { chunk++ }
+        _max = max(v, _max)
+        if _max == k { chunk++ }
     }
     return chunk
-}
-
-func isValid(arr []int, start int) bool {
-    sort.Ints(arr)
-    for i := start; i < len(arr); i++ {
-        if arr[i] != i { return false }
-    }
-    return true
 }
