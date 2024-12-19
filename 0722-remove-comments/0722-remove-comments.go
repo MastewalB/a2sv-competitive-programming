a97be79b2315c5f8d6346df9183bebcebf1f28f5
Mastewal Belete <52622856+MastewalB@users.noreply.github.com>
2024-12-19 12:03:38 +0300

    0722-REMOVE-COMMENTS Stats: Time: 0 ms (100%), Space: 4 MB (33.33%) - LeetHub

--- 0722-remove-comments/0722-remove-comments.go
+++ 0722-remove-comments/0722-remove-comments.go
@@ -1,33 +1,37 @@
 func removeComments(source []string) []string {
-    var temp strings.Builder
-    
-    str := strings.Join(source, "\\n") + "\\n"
-    i, n := 0, len(str)
+    var (
+        lineStr strings.Builder
+        block bool
+        n int = len(source)
+        curr int = 0
+    )
 
-    for i < n {
-        if i < n - 1 && str[i:i + 2] == "//" {
-            i = find(str, i + 2, "\\n")
-        } else if i < n - 1 && str[i:i + 2] == "/*" {
-            i = find(str, i + 2, "*/") + 2
-        } else {
-            temp.WriteString(string(str[i]))
-            i++
+    for i := 0; i < n; i++ {
+        line := source[i]
+        m := len(line)
+
+        for j := 0; j < m; {
+            if j < m - 1 && line[j:j + 2] == "//" && !block {
+                break
+            } else if j < m - 1 && line[j:j + 2] == "/*" && !block {
+                block = true
+                j += 2
+            } else if j < m - 1 && line[j:j + 2] == "*/" && block {
+                block = false
+                j += 2
+            } else {
+                if !block {
+                    lineStr.WriteString(string(line[j]))
+                }
+                j++
+            }
+        }
+        if !block && lineStr.Len() > 0 {
+            source[curr] = lineStr.String()
+            curr++
+            lineStr.Reset()
         }
     }
 
-    split := strings.Split(temp.String(), "\\n")
-    res := make([]string, 0)
-    for i := 0; i < len(split); i++ {
-        if split[i] != "" {
-            res = append(res, split[i])
-        }
-    }
-    return res
-}
-
-func find(s string, start int, t string) int {
-    for i := start; i <= len(s) - len(t); i++ {
-        if s[i: i + len(t)] == t { return i }
-    }
-    return -1
+    return source[:curr]
 }
