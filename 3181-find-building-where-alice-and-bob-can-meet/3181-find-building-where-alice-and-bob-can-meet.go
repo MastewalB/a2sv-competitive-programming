fe9fb9b345de8013ea603ed108f676fb12f2cc5f
Mastewal Belete <52622856+MastewalB@users.noreply.github.com>
2024-12-27 09:48:27 +0300

    3181-FIND-BUILDING-WHERE-ALICE-AND-BOB-CAN-MEET Stats: Time: 75 ms (42.56%), Space: 19.9 MB (32.4%) - LeetHub

--- 3181-find-building-where-alice-and-bob-can-meet/3181-find-building-where-alice-and-bob-can-meet.go
+++ 3181-find-building-where-alice-and-bob-can-meet/3181-find-building-where-alice-and-bob-can-meet.go
@@ -2,29 +2,29 @@ func leftmostBuildingQueries(heights []int, queries [][]int) []int {
     var (
         qLen int = len(queries)
         hLen int = len(heights)
-        lastProcessed int = hLen
         stack []int = make([]int, 0)
         res []int = make([]int, qLen)
+        queryMap = make(map[int][][]int)
     )
 
-    for _, v := range queries {
-        if v[0] > v[1] { v[1], v[0] = v[0], v[1]}
-    }
-    for i := 0; i < qLen; i++ { queries[i] = append(queries[i], i) }
-    sort.Slice(queries, func(x, y int) bool {
-        return queries[x][1] > queries[y][1]
-    })
-
-    for _, q := range queries {
-        x, y, qIndex := q[0], q[1], q[2]
-        for i := lastProcessed - 1; i > y; i-- {
-            for len(stack) > 0 && heights[i] >= heights[stack[len(stack) - 1]] { stack = stack[:len(stack) - 1] }
-            stack = append(stack, i) 
+    for i, v := range queries {
+        a, b := v[0], v[1]
+        if a > b { b, a = a, b }
+        if heights[a] < heights[b] || a == b {
+            res[i] = b
+        } else {
+            queryMap[b] = append(queryMap[b], []int{ a, i }) // queryMap[LargestIndex] = [[SmallerIndex, QueryIndex]....]
         }
-        if x == y || heights[x] < heights[y] {
-            res[qIndex] = y
-        } else { res[qIndex] = bSearch(heights, stack, heights[x]) }
-        lastProcessed = y + 1
+    }
+
+    for i := hLen - 1; i >= 0; i-- {
+        for _, v := range queryMap[i] {
+            h, index := v[0], v[1]
+            res[index] = bSearch(heights, stack, heights[h])
+        }
+
+        for len(stack) > 0 && heights[i] >= heights[stack[len(stack) - 1]] { stack = stack[:len(stack) - 1] }
+        stack = append(stack, i) 
     }
     return res
 }
