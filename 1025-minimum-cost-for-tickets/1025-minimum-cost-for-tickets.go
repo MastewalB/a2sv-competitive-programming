b0b1065a4edc38a09acef36bd2d53620e97b7c70
Mastewal Belete <52622856+MastewalB@users.noreply.github.com>
2024-12-31 16:21:01 +0300

    1025-MINIMUM-COST-FOR-TICKETS Stats: Time: 2 ms (17.33%), Space: 4.8 MB (12%) - LeetHub

--- 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.go
+++ 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.go
@@ -1,16 +1,17 @@
 func mincostTickets(days []int, costs []int) int {
-    var R func(int) int
-    year, cache := make([]int, 366), make(map[int]int)
+    var dp func(int) int
+    year, cache := make([]int, days[len(days) - 1] + 1), make(map[int]int)
 
     for _, v := range days { year[v] = 1 }
-    R = func(i int) int {
-        if val, ok := cache[i]; ok { return val }
-        if i > 365 { return 0 }
-        if year[i] == 0 { return R(i + 1) }
-        ans := min(R(i + 1) + costs[0], R(i + 7) + costs[1], R(i + 30) + costs[2])
-        cache[i] = ans
-        return ans
+    dp = func(i int) int {
+        if i >= len(year) { return 0 }
+        if year[i] == 0 { return dp(i + 1) }
+
+        if _, ok := cache[i]; !ok {
+            cache[i] = min(dp(i + 1) + costs[0], dp(i + 7) + costs[1], dp(i + 30) + costs[2])
+        }
+        return cache[i]
     }
 
-    return R(1)
+    return dp(1)
 }
