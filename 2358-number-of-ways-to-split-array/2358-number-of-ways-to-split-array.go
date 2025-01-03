483f868a802bc15d6f590cc4cdc9916f6cc9b1f9
Mastewal Belete <52622856+MastewalB@users.noreply.github.com>
2025-01-03 19:37:58 +0300

    2358-NUMBER-OF-WAYS-TO-SPLIT-ARRAY Stats: Time: 4 ms (35%), Space: 10.8 MB (60%) - LeetHub

--- 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.go
+++ 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.go
@@ -1,16 +1,17 @@
 func waysToSplitArray(nums []int) int {
     var (
-        ways, curr, sum int
+        ways int
         N int = len(nums)
+        prefix []int = make([]int, N)
     )
 
-    for _, v := range nums{
-        sum += v
+    prefix[0] = nums[0]
+    for k, v := range nums{
+        if k > 0 { prefix[k] = v + prefix[k - 1] }
     }
 
     for i := 0; i < N - 1; i++ {
-        curr += nums[i]
-        if curr >= sum - curr { ways++ }
+        if prefix[i] >= prefix[N - 1] - prefix[i] { ways++ }
     }
 
     return ways
